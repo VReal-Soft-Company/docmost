42af69336e3b7f92e4e53cd357a3ec2cb9897ee2
Igor Mostovenko <igormostovenko@gmail.com>
2024-11-21 17:52:54 -0800

    Update main.ts

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -10,42 +10,8 @@ import fastifyMultipart from '@fastify/multipart';
 import { WsRedisIoAdapter } from './ws/adapter/ws-redis.adapter';
 import { InternalLogFilter } from './common/logger/internal-log-filter';
 import fastifyCookie from '@fastify/cookie';
-import * as nodemailer from 'nodemailer';
 
 async function bootstrap() {
-
-  try {
-    console.log('host', process.env.SMTP_HOST);
-    console.log('port', Number(process.env.SMTP_PORT));
-    console.log('secure', process.env.SMTP_SECURE === 'true');
-    console.log('auth.user', process.env.SMTP_USERNAME);
-    console.log('auth.pass', process.env.SMTP_PASSWORD);
-    
-    const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: Number(process.env.SMTP_PORT),
-      secure: process.env.SMTP_SECURE === 'true',
-      auth: {
-        user: process.env.SMTP_USERNAME,
-        pass: process.env.SMTP_PASSWORD,
-      },
-      debug: true,
-      logger: true,
-    });
-
-    const info = await transporter.sendMail({
-      from: '"VReal Soft 👻" <developer@vrealsoft.com>', // sender address
-      to: 'igormostovenko@gmail.com', // list of receivers
-      subject: 'Hello ✔', // Subject line
-      text: 'Hello world?', // plain text body
-      html: '<b>Hello world?</b>', // html body
-    });
-
-    console.log('Message sent', info);
-  } catch (e) {
-    console.error(e);
-  }
-
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({
